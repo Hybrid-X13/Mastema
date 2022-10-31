c98213898e29892cf1899240f2c73ad65c16e727
Hybrid <infinitelyuda@gmail.com>
2022-10-31 12:51:10 -0400

    Changed damage var type from int to float

--- mastema_src/items/actives/raven_beak.lua
+++ mastema_src/items/actives/raven_beak.lua
@@ -11,11 +11,11 @@ function Item.evaluateCache(player, cacheFlag)
 	or SaveData.ItemData.RavenBeak.DMG > 0
 	then
 		if player:HasCollectible(CollectibleType.COLLECTIBLE_SOY_MILK) then
-			player.Damage = player.Damage + (0.01 * SaveData.ItemData.RavenBeak.DMG * 0.2)
+			player.Damage = player.Damage + (SaveData.ItemData.RavenBeak.DMG * 0.2)
 		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_ALMOND_MILK) then
-			player.Damage = player.Damage + (0.01 * SaveData.ItemData.RavenBeak.DMG * 0.33)
+			player.Damage = player.Damage + (SaveData.ItemData.RavenBeak.DMG * 0.33)
 		else
-			player.Damage = player.Damage + (0.01 * SaveData.ItemData.RavenBeak.DMG)
+			player.Damage = player.Damage + SaveData.ItemData.RavenBeak.DMG
 		end
 	end
 end
@@ -40,30 +40,30 @@ function Item.useItem(item, rng, player, flags, activeSlot, customVarData)
 			then
 				if pickup.Variant == PickupVariant.PICKUP_HEART then
 					if pickup.SubType == HeartSubType.HEART_ETERNAL then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 25
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.25
 					elseif pickup.SubType == HeartSubType.HEART_BLACK
 					or pickup.SubType == HeartSubType.HEART_GOLDEN
 					or pickup.SubType == HeartSubType.HEART_BONE
 					then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 15
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.15
 					elseif pickup.SubType == HeartSubType.HEART_SOUL then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 10
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.1
 					elseif pickup.SubType == HeartSubType.HEART_BLENDED then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 7
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.07
 					elseif pickup.SubType == HeartSubType.HEART_HALF_SOUL then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 5
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.05
 					elseif pickup.SubType == HeartSubType.HEART_DOUBLEPACK then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 4
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.04
 					elseif pickup.SubType == HeartSubType.HEART_FULL
 					or pickup.SubType == HeartSubType.HEART_ROTTEN
 					then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 2
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.02
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 1
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.01
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_COIN then
 					if pickup.SubType == CoinSubType.COIN_GOLDEN then
-						local dmg = 1
+						local dmg = 0.01
 						
 						for i = 1, 99 do
 							local randNum = rng:RandomInt(10)
@@ -71,70 +71,70 @@ function Item.useItem(item, rng, player, flags, activeSlot, customVarData)
 							if randNum == 0 then
 								break
 							else
-								dmg = dmg + 1
+								dmg = dmg + 0.01
 							end
 						end
 						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + dmg
 					elseif pickup.SubType == CoinSubType.COIN_DIME then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 10
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.1
 					elseif pickup.SubType == CoinSubType.COIN_LUCKYPENNY then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 8
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.08
 					elseif pickup.SubType == CoinSubType.COIN_NICKEL
 					or pickup.SubType == CoinSubType.COIN_STICKYNICKEL
 					then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 5
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.05
 					elseif pickup.SubType == CoinSubType.COIN_DOUBLEPACK then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 2
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.02
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 1
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.01
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_KEY then
 					if pickup.SubType == KeySubType.KEY_GOLDEN then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 12
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.12
 					elseif pickup.SubType == KeySubType.KEY_CHARGED then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 8
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.08
 					elseif pickup.SubType == KeySubType.KEY_DOUBLEPACK then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 4
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.04
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 2
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.02
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_BOMB then
 					if pickup.SubType == BombSubType.BOMB_GIGA then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 50
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.5
 					elseif pickup.SubType == BombSubType.BOMB_GOLDEN then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 12
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.12
 					elseif pickup.SubType == BombSubType.BOMB_DOUBLEPACK then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 4
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.04
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 2
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.02
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_PILL then
 					if pickup.SubType == PillColor.PILL_GOLD + PillColor.PILL_GIANT_FLAG then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 50
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.5
 					elseif pickup.SubType == PillColor.PILL_GOLD then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 25
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.25
 					elseif pickup.SubType > PillColor.PILL_GIANT_FLAG then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 10
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.1
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 5
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.05
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_LIL_BATTERY then
 					if pickup.SubType == BatterySubType.BATTERY_MEGA
 					or pickup.SubType == BatterySubType.BATTERY_GOLDEN
 					then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 25
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.25
 					elseif pickup.SubType == BatterySubType.BATTERY_NORMAL then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 8
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.08
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 4
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.04
 					end
 				elseif pickup.Variant == PickupVariant.PICKUP_TAROTCARD then
-					SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 7
+					SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.07
 				elseif pickup.Variant == PickupVariant.PICKUP_TRINKET then
 					if pickup.SubType > TrinketType.TRINKET_GOLDEN_FLAG then
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 16
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.16
 					else
-						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 8
+						SaveData.ItemData.RavenBeak.DMG = SaveData.ItemData.RavenBeak.DMG + 0.08
 					end
 				end
 				
