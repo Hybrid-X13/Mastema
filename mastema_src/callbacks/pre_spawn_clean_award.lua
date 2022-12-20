922cc990a96d260a4c0f0d3a9bd534a1d3b33e32
Hybrid <infinitelyuda@gmail.com>
2022-12-20 17:41:59 -0500

    Added Devil's Bargain

--- mastema_src/callbacks/pre_spawn_clean_award.lua
+++ mastema_src/callbacks/pre_spawn_clean_award.lua
@@ -1,6 +1,9 @@
+local DevilsBargain = require("mastema_src.items.actives.devils_bargain")
 local BookOfJubilees = require("mastema_src.items.passives.book_of_jubilees")
 
 local function MC_PRE_SPAWN_CLEAN_AWARD()
+	DevilsBargain.preSpawnCleanAward()
+	
 	BookOfJubilees.preSpawnCleanAward()
 end
 
