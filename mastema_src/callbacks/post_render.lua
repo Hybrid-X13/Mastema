3b183d9b67235671c33d7ae3dcbc01cf33cf9926
Hybrid <infinitelyuda@gmail.com>
2023-01-05 13:54:15 -0500

    Removed Mastema's Wrath

--- mastema_src/callbacks/post_render.lua
+++ mastema_src/callbacks/post_render.lua
@@ -1,12 +1,9 @@
 local T_Mastema = require("mastema_src.characters.t_mastema")
-local MastemasWrath = require("mastema_src.items.passives.mastemas_wrath")
 local ShatteredSoul = require("mastema_src.items.trinkets.shattered_soul")
 local SatanicRitual = require("mastema_src.misc.satanic_ritual")
 
 local function MC_POST_RENDER()
 	T_Mastema.postRender()
-	
-	MastemasWrath.postRender()
 
 	ShatteredSoul.postRender()
 	
