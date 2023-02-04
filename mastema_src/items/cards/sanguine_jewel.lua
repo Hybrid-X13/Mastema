ffa396bd342a626896ce34dc6c29e209b9fa6828
Hybrid <infinitelyuda@gmail.com>
2023-02-04 14:14:39 -0500

    Fixed dmg and applying transformation

--- mastema_src/items/cards/sanguine_jewel.lua
+++ mastema_src/items/cards/sanguine_jewel.lua
@@ -1,35 +1,54 @@
 local Enums = require("mastema_src.enums")
-local SaveData = require("mastema_src.savedata")
 local game = Game()
-local sfx = SFXManager()
 local rng = RNG()
 
 local Consumable = {}
 
-function Consumable.evaluateCache(player, cacheFlag)
-	if cacheFlag == CacheFlag.CACHE_DAMAGE then
-		if player:HasCollectible(CollectibleType.COLLECTIBLE_SOY_MILK) then
-			player.Damage = player.Damage + (0.5 * SaveData.ItemData.SanguineJewel.DMG * 0.2)
-		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_ALMOND_MILK) then
-			player.Damage = player.Damage + (0.5 * SaveData.ItemData.SanguineJewel.DMG * 0.3)
-		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_20_20) then
-			player.Damage = player.Damage + (0.5 * SaveData.ItemData.SanguineJewel.DMG * 0.8)
-		else
-			player.Damage = player.Damage + (0.5 * SaveData.ItemData.SanguineJewel.DMG)
-		end
+local transformationMap = {
+	[PlayerForm.PLAYERFORM_GUPPY] = CollectibleType.COLLECTIBLE_GUPPYS_TAIL,
+	[PlayerForm.PLAYERFORM_LORD_OF_THE_FLIES] = CollectibleType.COLLECTIBLE_MULLIGAN,
+	[PlayerForm.PLAYERFORM_MUSHROOM] = CollectibleType.COLLECTIBLE_GODS_FLESH,
+	[PlayerForm.PLAYERFORM_ANGEL] = CollectibleType.COLLECTIBLE_MITRE,
+	[PlayerForm.PLAYERFORM_BOB] = CollectibleType.COLLECTIBLE_IPECAC,
+	[PlayerForm.PLAYERFORM_DRUGS] = CollectibleType.COLLECTIBLE_VIRUS,
+	[PlayerForm.PLAYERFORM_MOM] = CollectibleType.COLLECTIBLE_MOMS_EYE,
+	[PlayerForm.PLAYERFORM_BABY] = CollectibleType.COLLECTIBLE_BROTHER_BOBBY,
+	[PlayerForm.PLAYERFORM_EVIL_ANGEL] = CollectibleType.COLLECTIBLE_BRIMSTONE,
+	[PlayerForm.PLAYERFORM_POOP] = CollectibleType.COLLECTIBLE_E_COLI,
+	[PlayerForm.PLAYERFORM_BOOK_WORM] = CollectibleType.COLLECTIBLE_BOOK_OF_VIRTUES,
+	[PlayerForm.PLAYERFORM_SPIDERBABY] = CollectibleType.COLLECTIBLE_SPIDER_BITE,
+}
+
+local function AddTransformation(player, transformation)
+	if transformationMap[transformation] == nil then return end
+
+	for i = 1, 3 do
+		player:AddCollectible(transformationMap[transformation])
+		player:RemoveCollectible(transformationMap[transformation], true, 0, false)
 	end
-	
-	if cacheFlag == CacheFlag.CACHE_FLYING
-	and not player:HasCollectible(Enums.Collectibles.TORN_WINGS)
-	and SaveData.ItemData.SanguineJewel.Leviathan
-	then
-		player.CanFly = true
+end
+
+function Consumable.evaluateCache(player, cacheFlag)
+	if cacheFlag ~= CacheFlag.CACHE_DAMAGE then return end
+
+	local tempEffects = player:GetEffects()
+	local effectCount = tempEffects:GetCollectibleEffectNum(Enums.Collectibles.SANGUINE_JEWEL_DMG_NULL)
+
+	if player:HasCollectible(CollectibleType.COLLECTIBLE_SOY_MILK) then
+		player.Damage = player.Damage + (0.5 * effectCount * 0.2)
+	elseif player:HasCollectible(CollectibleType.COLLECTIBLE_ALMOND_MILK) then
+		player.Damage = player.Damage + (0.5 * effectCount * 0.3)
+	elseif player:HasCollectible(CollectibleType.COLLECTIBLE_20_20) then
+		player.Damage = player.Damage + (0.5 * effectCount * 0.8)
+	else
+		player.Damage = player.Damage + (0.5 * effectCount)
 	end
 end
 
 function Consumable.useCard(card, player, flag)
 	if card ~= Enums.Cards.SANGUINE_JEWEL then return end
 
+	local tempEffects = player:GetEffects()
 	local rng = player:GetCardRNG(Enums.Cards.SANGUINE_JEWEL)
 	local randNum = rng:RandomInt(100)
 
@@ -39,18 +58,10 @@ function Consumable.useCard(card, player, flag)
 	if flag & UseFlag.USE_MIMIC ~= UseFlag.USE_MIMIC then
 		player:AnimateCard(Enums.Cards.SANGUINE_JEWEL, "UseItem")
 	end
-
+	
 	if randNum < 2 then
-		if not player:HasPlayerForm(PlayerForm.PLAYERFORM_EVIL_ANGEL)
-		and not SaveData.ItemData.SanguineJewel.Leviathan
-		then
-			SaveData.ItemData.SanguineJewel.Leviathan = true
-			game:GetHUD():ShowItemText("Leviathan!")
-			sfx:Play(SoundEffect.SOUND_POWERUP_SPEWER)
-			player:AddPlayerFormCostume(PlayerForm.PLAYERFORM_EVIL_ANGEL)
-			player:AddBlackHearts(4)
-			player:AddCacheFlags(CacheFlag.CACHE_FLYING)
-			player:EvaluateItems()
+		if not player:HasPlayerForm(PlayerForm.PLAYERFORM_EVIL_ANGEL) then
+			AddTransformation(player, PlayerForm.PLAYERFORM_EVIL_ANGEL)
 		else
 			for i = 1, 2 do
 				local pos = Isaac.GetFreeNearPosition(player.Position, 40)
@@ -73,7 +84,7 @@ function Consumable.useCard(card, player, flag)
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_PENNY, pos, Vector.Zero, nil)
 		end
 	elseif randNum < 65 then
-		SaveData.ItemData.SanguineJewel.DMG = SaveData.ItemData.SanguineJewel.DMG + 1
+		tempEffects:AddCollectibleEffect(Enums.Collectibles.SANGUINE_JEWEL_DMG_NULL)
 		player:AddCacheFlags(CacheFlag.CACHE_DAMAGE)
 		player:EvaluateItems()
 	end
