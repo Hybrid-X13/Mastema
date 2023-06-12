3a4bba51960cecb117819112dea0a03532a67881
Hybrid <infinitelyuda@gmail.com>
2023-06-12 13:35:54 -0400

    Added sound effect

--- mastema_src/items/cards/prayer_of_repentance.lua
+++ mastema_src/items/cards/prayer_of_repentance.lua
@@ -1,6 +1,7 @@
 local Enums = require("mastema_src.enums")
 local Functions = require("mastema_src.functions")
 local rng = RNG()
+local sfx = SFXManager()
 
 local Consumable = {}
 
@@ -11,6 +12,7 @@ function Consumable.useCard(card, player, flag)
 	local pos = Isaac.GetFreeNearPosition(player.Position, 40)
 	local confessional = Isaac.Spawn(EntityType.ENTITY_SLOT, Enums.Slots.CONFESSIONAL, 0, pos, Vector.Zero, nil)
 	Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, confessional.Position, Vector.Zero, confessional)
+	sfx:Play(SoundEffect.SOUND_UNHOLY, 1, 2, false, 0.9)
 
 	Functions.PlayVoiceline(Enums.Voicelines.PRAYER_OF_REPENTANCE, flag, rng:RandomInt(2))
 end
